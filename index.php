b3cf8674f5ad8fde75220bc372b47aa7a22fd85d
Rémi Bèges <remi.beges@gmail.com>
2016-05-12 18:24:51 +0200

    Refactoring

--- index.php
+++ index.php
@@ -2,27 +2,5 @@
 session_start();
 $_SESSION['token'] = md5(session_id() . time());
 $token = $_SESSION['token'];
+include_once('welcome.php');
 ?>
-
-<html>
-<body>
-  <script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
-  <script>
-  function sendMail()//Use jquery to do ajax request, easier than diretly in javascript 
-  {
-    $.post('postMail.php',{token:$("#token").val(), mail:$("#mail").val()}).done(function(data) {
-      $('#complete').html(data);
-    }
-  );
-}
-</script>
-
-<form>
-  <label for="mail">Mail:</label>	 <input type="text" name="mail" id="mail" />
-  <input type="hidden" id="token" name="token" value="<?php echo $token ?>" />
-  <input type="button" value="submit" onClick="sendMail();" />
-  <p id="complete"> </p>
-</form>
-
-</body>
-</html>
