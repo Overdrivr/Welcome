c4d61e066bac9b6f7de95af62b5c8a610cf90c66
Rémi Bèges <remi.beges@gmail.com>
2016-05-12 18:24:32 +0200

    Fix issue with sql request

--- postMail.php
+++ postMail.php
@@ -16,7 +16,7 @@ if(isset($_SESSION['token']) && isset($_POST['token']) && isset($_POST['mail']))
 			if($result->num_rows == 0) //Check the mail is not already present
 			{
 				echo "ok";
-				$sql = "INSERT INTO mails  VALUES ('', '".$mail."', NOW())";
+				$sql = "INSERT INTO mails  VALUES (NULL, '".$mail."', NOW())";
 				if(!$result = $db->query($sql)){
 					die('There was an error running the query [' . $db->error . ']');
 				}
