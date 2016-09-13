f64c1fe47631215e96a37ed1f6427a9fb3712f67
Rémi Bèges <remi.beges@gmail.com>
2016-09-13 14:57:58 +0200

    Add twitter timeline

--- welcome.php
+++ welcome.php
@@ -1,9 +1,3 @@
-<?php
-session_start();
-$token = uniqid(rand(), true);
-$_SESSION['token'] = $token;
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,124 +16,132 @@ $_SESSION['token'] = $token;
 </head>
 <body>
   <div id="section1">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <img id="logo" src="logo_v3.svg" class="img-responsive center-block svg" alt="micro-ci logo" >
-          <p class="text-center" id="subtitle">Continuous integration for embedded development</p>
-          <h3 class="text-center">micro-ci lets you execute build jobs, run test suites on embedded hardware, and deploy changes at each commit.</h3>
-        </div>
-        <!-- Double column bullet points -->
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+      <img id="logo" src="logo_v3.svg" class="img-responsive center-block svg" alt="micro-ci logo" >
+      <p class="text-center" id="subtitle">Continuous integration for embedded development</p>
+      <h3 class="text-center">micro-ci lets you execute build jobs, run test suites on embedded hardware, and deploy changes at each commit.</h3>
+</div>
+      <!-- Double column bullet points -->
 
-        <div class="col-md-6">
-          <i class="ion-gear-a" aria-hidden="true"></i>
-          <span>Build embedded code for any platform</span>
-        </div>
-
-        <div class="col-md-6">
-          <i class="ion-bug" aria-hidden="true"></i>
-          <span>Test your code on real hardware</span>
-        </div>
-
-        <div class="col-md-6">
-          <i class="ion-social-github" aria-hidden="true"></i>
-          <span>Seamless integration with version control systems</span>
-        </div>
-
-        <div class="col-md-6">
-          <i class="ion-monitor" aria-hidden="true"></i>
-          <span>Test your code on virtual embedded machines</span>
-        </div>
-
-        <div class="col-md-6">
-          <i class="ion-stats-bars" aria-hidden="true"></i>
-          <span>Detailed metrics about built firmwares</span>
-        </div>
-
-        <div class="col-md-6">
-          <i class="ion-paper-airplane" aria-hidden="true"></i>
-          <span>Deploy embedded code with confidence</span>
-        </div>
-
+      <div class="col-md-5 col-md-offset-1">
+        <i class="ion-gear-a" aria-hidden="true"></i>
+        <span>Build embedded code for any platform</span>
       </div>
-    </div> <!-- end of section 1 -->
-  </div>
+
+      <div class="col-md-5 col-md-offset-1">
+        <i class="ion-bug" aria-hidden="true"></i>
+        <span>Test your code on real hardware</span>
+      </div>
+
+      <div class="col-md-5 col-md-offset-1">
+        <i class="ion-social-github" aria-hidden="true"></i>
+        <span>Seamless integration with version control systems</span>
+      </div>
+
+      <div class="col-md-5 col-md-offset-1">
+        <i class="ion-monitor" aria-hidden="true"></i>
+        <span>Test your code on virtual embedded machines</span>
+      </div>
+
+      <div class="col-md-5 col-md-offset-1">
+        <i class="ion-stats-bars" aria-hidden="true"></i>
+        <span>Detailed metrics about built firmwares</span>
+      </div>
+
+      <div class="col-md-5 col-md-offset-1">
+        <i class="ion-paper-airplane" aria-hidden="true"></i>
+        <span>Deploy embedded code with confidence</span>
+      </div>
+
+    </div>
+  </div> <!-- end of section 1 -->
+</div>
 
   <div class="container">
     <div class="row" id="section2">
-      <div class="col-md-6 vcenter">
-        <div> <h2>Be notified of major updates</h2> </div>
-      </div> <!--
-      -->
-      <div class="col-md-6 vcenter">
-        <div> <p> Foooobar </p> </div>
-        <!--
-        <form class="form-inline" onsubmit=" return false;">
+      <div class="col-md-12">
+        <h2 class="text-center">Stay in touch and be notified of the latest updes</h2>
+      </div>
+
+      <div class="col-xs-6">
+        <a href="https://twitter.com/micro_ci">
+          <i class="ion-social-twitter" ></i>
+          @micro-ci
+        </a>
+      </div>
+
+      <div class="col-xs-6">
+        <form class="form-inline text-center" >
           <div class="input-group">
-            <input type="mail" class="form-control" id="mail" name="mail" placeholder="e-mail">
+            <input type="text" class="form-control" id="mail" name="mail" placeholder="e-mail">
             <span class="input-group-btn">
-              <button class="btn btn-default" type="submit" id="send" onclick="sendMail();">submit</button>
+              <button class="btn btn-default" type="submit" onClick="sendMail();">submit</button>
             </span>
-          </div>
-          <input type="hidden" id="token" name="token" value="<?php echo $token; ?>" />
+          </div><!-- /input-group -->
+          <input type="hidden" id="token" name="token" value="<?php echo $token ?>" />
           <p id="complete"> </p>
+
         </form>
-      -->
       </div>
 
     </div>
 
     <div class="row">
-      <a class="twitter-timeline" href="https://twitter.com/micro_ci">Tweets by micro_ci</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-    </div>
+      <a class="twitter-timeline"
+        href="https://twitter.com/{micro_ci}">
+      Tweets by @{micro-ci}
+      </a>
+    </div> <!-- end of section 2 -->
   </div>
 
   <div id="description">
 
     <div id="build_test">
-      <div class="container">
-        <div class="row" >
-          <div class="col-md-6">
-            <h3 class="text-center"> <i class="ion-gear-a" aria-hidden="true"></i>
-              <span>Build embedded code for any platform</span></h3>
+    <div class="container">
+
+        <div class="col-md-6">
+          <h3 class="text-center"> <i class="ion-gear-a" aria-hidden="true"></i>
+            <span>Build embedded code for any platform</span></h3>
               <p>
                 Automatically build your code on each version (commit, tag, release) and immediately know when the build is failing.
                 Build your code in parallel for multiple platforms.
               </p>
 
-            </div>
+          </div>
 
-            <div class="col-md-6">
-              <h3 class="text-center"> <i class="ion-bug" aria-hidden="true"></i>
-                <span>Test your embedded code on real hardware</span></h3>
+          <div class="col-md-6">
+            <h3 class="text-center"> <i class="ion-bug" aria-hidden="true"></i>
+              <span>Test your embedded code on real hardware</span></h3>
 
                 <p>
                   Run your test suites on actual microcontroller hosted on our premises.
                   Validate your code in near-production conditions while enjoying the comfort of a continuous integration environment.
                 </p>
 
-              </div>
+            </div>
 
 
-            </div> <!-- end of build_test -->
+          </div> <!-- end of build_test -->
 
-          </div>
         </div>
-        <div id="version_virtual">
+
+    <div id="version_virtual">
           <div class="container">
-            <div class="row" >
-              <div class="col-md-6">
-                <h3><i class="ion-social-github" aria-hidden="true"></i>
-                  <span>Seamless integration with version control systems</span></h3>
+          <div class="row" >
+            <div class="col-md-6">
+              <h3><i class="ion-social-github" aria-hidden="true"></i>
+                <span>Seamless integration with version control systems</span></h3>
                   <p>
                     Add easily a code repository hosted on github or bitbucket under continuous integration. Micro-ci is notified each time you commit and runs your builds, automatically.
                   </p>
 
-                </div>
+              </div>
 
-                <div class="col-md-6">
-                  <h3>   <i class="ion-monitor" aria-hidden="true"></i>
-                    <span>Test your code on virtual embedded machines</span></h3>
+              <div class="col-md-6">
+                <h3>   <i class="ion-monitor" aria-hidden="true"></i>
+                  <span>Test your code on virtual embedded machines</span></h3>
 
                     <p>
                       Test code in virtual embedded environments. Stimulate and check I/Os and validate embedded functionalities.
@@ -148,29 +150,29 @@ $_SESSION['token'] = $token;
 
 
 
-                </div> <!-- end of version_virtual -->
-              </div>
+              </div> <!-- end of version_virtual -->
             </div>
+          </div>
 
-            <div id="metrics_deploy">
-              <div class="container">
-                <div class="row">
+<div id="metrics_deploy">
+<div class="container">
+              <div class="row">
 
 
-                  <div class="col-md-6">
-                    <h3>  <i class="ion-stats-bars" aria-hidden="true"></i>
-                      <span>Detailed metrics about built firmwares</span></h3>
+                <div class="col-md-6">
+                  <h3>  <i class="ion-stats-bars" aria-hidden="true"></i>
+                    <span>Detailed metrics about built firmwares</span></h3>
 
                       <p>
                         Metrics are recorded for each build such as memory usage, stack usage, code performance and many more.
                         Check the evolution of all these metrics during the development and life of your product.
                       </p>
 
-                    </div>
+                  </div>
 
-                    <div class="col-md-6">
-                      <h3> <i class="ion-paper-airplane" aria-hidden="true"></i>
-                        <span>Deploy embedded code with confidence</span></h3>
+                  <div class="col-md-6">
+                    <h3> <i class="ion-paper-airplane" aria-hidden="true"></i>
+                      <span>Deploy embedded code with confidence</span></h3>
 
                         <p>
                           When a build is successful, deploy with confidence the new firmware to a fleet of devices.
@@ -178,28 +180,40 @@ $_SESSION['token'] = $token;
 
 
 
-                      </div> <!-- end of version_virtual -->
-                    </div>
+                  </div> <!-- end of version_virtual -->
+                </div>
+              </div>
+
+
+
+              <div class="container">
+                <div class="row" id="milestones">
+
+                  <div class="col-md-12">
+                    <h2>Milestones</h2>
+                    <p>First build platform prototype</p>
+                    <p>Production testing</p>
+                    <p>Beta - Virtualized jobs</p>
+                    <p>Alpha - Virtualized jobs</p>
+                    <p>First release</p>
+                    <p>Beta - True hardware jobs</p>
+                    <p>Alpha - True hardware jobs</p>
+                    <p>Full release !</p>
                   </div>
                 </div>
               </div>
+              <!-- end of master container -->
+
+
+
 
               <script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
               <script>
               function sendMail()//Use jquery to do ajax request, easier than diretly in javascript
               {
-
                 $.post('postMail.php',{token:$("#token").val(), mail:$("#mail").val()})
                 .done(function(data) {
-                  if(data == "ok")
-                  {
-                    $('#send').attr('class', 'btn btn-success disabled');
-                    $('#send').attr('class', 'btn btn-success disabled').prop('disabled', true);
-                  }
-                  else {
-                    alert(data);
-                      $('#send').attr('class', 'btn btn-danger');
-                  }
+                  $('#complete').html(data);
                 });
               }
 
@@ -239,5 +253,6 @@ $_SESSION['token'] = $token;
               </script>
               <!-- Latest compiled and minified JavaScript -->
               <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
             </body>
             </html>
